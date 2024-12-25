ca09b41d618cee1f97423ff0f524c2cc00203ede
thiyangt <tstalagala@gmail.com>
2024-12-25 18:41:53 +0530

    updated

--- xmas2024_2.R
+++ xmas2024_2.R
@@ -5,22 +5,16 @@ library(ggplot2)
 font_add_google("Dancing Script", "dancing") 
 showtext_auto() 
 
-# Load necessary libraries
-library(ggplot2)
-library(showtext)
-
 # Add a fancy font
 font_add_google("Dancing Script", "dancing")  # Example of a fancy font from Google Fonts
 showtext_auto()  # Enable custom fonts
 
 # Set seed and create the canvas
-set.seed(8)
+set.seed(15)
 canvas <- canvas_flame(
-  colors = c("#ab00ff", "#8300c4", "#4c00a4",
-              "#31004a","#33007b",
-             "#FFC0CB", "#FFB6C1",
-             "#FF69B4", "#FF1493"),
-  symmetry = 5
+  colors = c("yellow", "forestgreen",  "green"),
+  symmetry = 5,
+  variation = 2
 
 )
 
@@ -29,21 +23,21 @@ canvas <- canvas +
   # Main greeting text
   annotate(
     "text",
-    x = 500, y = 500,  # Adjust coordinates to center on your canvas
+    x = 500, y = 150,  # Adjust coordinates to center on your canvas
     label = "Merry Christmas",
-    color = "green",
-    size = 20,  # Adjust size for prominence
+    color = "#FF69B4",
+    size = 12,  # Adjust size for prominence
     family = "dancing",  # Fancy font
     fontface = "bold"
   ) +
   # Footnote or credit
   annotate(
     "text",
-    x = 500, y = 300,  # Adjust coordinates for positioning below the main text
+    x = 500, y = 70,  # Adjust coordinates for positioning below the main text
     label = "R-Ladies, Colombo, Sri Lanka",
     color = "magenta",
     family = "dancing",
-    size = 10,  
+    size = 8,  
     fontface = "italic",
     fontface = "bold"
   )
